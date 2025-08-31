dd06dcb97da5922fa11d2f83b77a9eda27bc8c56
Егор <egor5f421@mail.ru>
2025-08-31 13:50:12 +0300

    feat: Добавлена отмена хода

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -81,6 +81,12 @@ class Program
                     char input = Console.ReadKey().KeyChar;
 
                     if (input == 'Q') return;
+                    if (input == 'U')
+                    {
+                        if (game.ReadPlayerLevel(game.IsXTurn() ? O : X) != HUMAN) game.Undo();
+                        game.Undo();
+                        break;
+                    }
 
                     if (game.IsLegalMove(input - '1'))
                     {
