d713ce2c28832b9a6ea0caff99a160d5a141a9be
Егор <egor5f421@mail.ru>
2025-08-31 19:29:21 +0300

    bot: Создан бот DrawBot

--- Tic-Tac-Toe/TicTacToe.cs
+++ Tic-Tac-Toe/TicTacToe.cs
@@ -25,6 +25,7 @@ public partial class TicTacToe
         ("Худший бот", typeof(WorstBot)),
         ("Случайный бот", typeof(RandomBot)),
         ("Драматург", typeof(DramatistBot)),
+        ("Ничья-бот", typeof(DrawBot)),
         ("Стандартный бот", typeof(BestBot)),
         ("X-Схема", typeof(XAndOSchemeBot)),
         ("MiniMax бот", typeof(MiniMaxBot)),
