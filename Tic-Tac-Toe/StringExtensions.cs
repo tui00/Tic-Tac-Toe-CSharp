d8f8d38db5980ce8176b985259b4af89a3b78424
Егор <egor5f421@mail.ru>
2025-09-04 16:43:18 +0300

    add: Добавлен метод FormatToTable для форматирования строк в таблицу

---
+++ Tic-Tac-Toe/StringExtensions.cs
@@ -0,0 +1,26 @@
+namespace TicTacToe;
+
+internal static class StringArrayExtensions
+{
+    public static string FormatToTable(this string[] columns, string separator = "   ")
+    {
+        string[][] columnsLines = [.. columns.Select(column => column.Split('\n'))];
+        int[] columnsWidths = [.. columnsLines.Select(lines => lines.Max(line => line.Length))];
+        int linesCount = columnsLines.Max(lines => lines.Length);
+
+        List<string> resultLines = [];
+        for (int i = 0; i < linesCount; i++)
+        {
+            resultLines.Add(string.Join(separator,
+                columnsLines.Select((columnLines, column) =>
+                    {
+                        string cell = i < columnLines.Length ? columnLines[i] : "";
+                        return cell.PadRight(columnsWidths[column], ' ');
+                    }
+                )
+            ));
+        }
+
+        return string.Join("\n", resultLines);
+    }
+}
