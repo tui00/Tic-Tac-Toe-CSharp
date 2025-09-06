7c5ff46d52360a36e33e18b4fa69c2349ffc95ac
Егор <egor5f421@mail.ru>
2025-09-06 13:49:52 +0300

    fix: Удалены \r и добавлен \n в Logger для корректного вывода логов

--- Tic-Tac-Toe/Logger.cs
+++ Tic-Tac-Toe/Logger.cs
@@ -3,6 +3,6 @@ namespace TicTacToe;
 public class Logger
 {
     public string log = "";
-    public void AddColumn(string column) => log = log.FormatToTable(column);
-    public void Log(string log) => this.log += log;
+    public void AddColumn(string column) => log = log.FormatToTable(column.Replace("\r", ""));
+    public void Log(string log) => this.log += log.Replace("\r", "") + "\n";
 }
