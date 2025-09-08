0e774299f78958628e22d3ac21c70d9efbd81cba
Егор <egor5f421@mail.ru>
2025-09-08 10:01:06 +0300

    feat(cli): implement game creation via API and init join flow

--- TicTacToe.Cli/Program.cs
+++ TicTacToe.Cli/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace TicTacToe.Cli;
+﻿using System.Net.Http.Json;
+
+namespace TicTacToe.Cli;
 
 class Program
 {
-    internal static void Main(string[] args)
+    internal static async Task Main(string[] args)
     {
-        Console.WriteLine(ReadVisualBoard("xo-xo-x--"));
+        using HttpClient client = new();
+        string[] config = File.ReadAllText("config").Replace("\r", "").Split('\n');
+        client.BaseAddress = new(config[0]);
+
+        Console.WriteLine("Привет! Введите (c)reate что-бы создать игру, (j)oin что-бы присоединиться, (q)uit для выхода: ");
+        Guid? joinCode = null;
+        while (joinCode == null)
+        {
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case 'c': joinCode = await Create(client); break;
+                case 'j': joinCode = Join(client); break;
+                case 'q': return;
+                default: continue;
+            }
+            break;
+        }
+        Console.WriteLine(joinCode);
+    }
+
+    internal static Guid Join(HttpClient client)
+    {
+        return Guid.Empty;
+    }
+
+    internal static async Task<Guid> Create(HttpClient client)
+    {
+        HttpResponseMessage responseMessage = await client.PostAsJsonAsync<NewGameRequest>("game/new", new(0, 0));
+        if (responseMessage != null && responseMessage.IsSuccessStatusCode)
+        {
+            NewGameResponse? response = await responseMessage.Content.ReadFromJsonAsync<NewGameResponse>();
+            if (response != null)
+            {
+                return response.Id;
+            }
+        }
+        throw new HttpRequestException("Не удалось создать новую игру. Сервер вернул ошибку или некорректный ответ.");
     }
 
     internal static string ReadVisualBoard(string board)
@@ -22,3 +60,9 @@ class Program
         return result;
     }
 }
+
+public record NewGameRequest(uint XLevel, uint OLevel);
+public record NewGameResponse(Guid Id);
+
+public record GameResponse(string Board, int Turn, uint Winner);
+public record MakeTurnRequest(int Cell);
