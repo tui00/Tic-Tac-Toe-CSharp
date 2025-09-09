2e3fd5ad00fede60935eff17d58f152c56578b6d
Егор <egor5f421@mail.ru>
2025-09-09 15:51:45 +0300

    feat(api): introduce GameState to track connected players and improve game state management

--- TicTacToe.Api/Controllers/GameController.cs
+++ TicTacToe.Api/Controllers/GameController.cs
@@ -17,11 +17,11 @@ public class GameController(IMemoryCache cache) : ControllerBase
     public IActionResult CreateGame([FromBody] NewGameRequest newGame)
     {
         Game game = new(newGame.XLevel, newGame.OLevel);
-        Guid id = Guid.NewGuid();
-        _cache.Set(id, game, TimeSpan.FromMinutes(30));
-        _usedGuids.Add(id);
+        Guid guid = Guid.NewGuid();
+        _cache.Set(guid, new GameState(game, 0), TimeSpan.FromMinutes(30));
+        _usedGuids.Add(guid);
 
-        return Ok(new NewGameResponse(id));
+        return Ok(new NewGameResponse(guid));
     }
 
     // GET /api/game/list
@@ -36,43 +36,46 @@ public class GameController(IMemoryCache cache) : ControllerBase
     [HttpGet("{id}/isLegal/{cell}")]
     public IActionResult IsLegal(Guid id, int cell)
     {
-        if (!_cache.TryGetValue(id, out Game? game))
+        if (!_cache.TryGetValue(id, out GameState? state))
             return NotFound(new { error = "Game not found" });
 
-        return Ok(new IsLegalResponse(game!.IsLegalMove(cell)));
+        return Ok(new IsLegalResponse(state!.Game.IsLegalMove(cell)));
     }
 
     // GET /api/game/{id}
     [HttpGet("{id}")]
     public IActionResult GetGame(Guid id)
     {
-        if (!_cache.TryGetValue(id, out Game? game))
+        if (!_cache.TryGetValue(id, out GameState? state))
             return NotFound(new { error = "Game not found" });
 
-        return Ok(new GameResponse(FormatBoard(game!), game!.ReadWhoseTurn(), game.ReadWinner()));
+        return Ok(new GameResponse(FormatBoard(state!), state!.Game.ReadWhoseTurn(), state.Game.ReadWinner(), state.ConnectedPlayers));
     }
 
     // POST /api/game/{id}
     [HttpPost("{id}")]
     public IActionResult MakeMove(Guid id, [FromBody] MakeTurnRequest request)
     {
-        if (!_cache.TryGetValue(id, out Game? game))
+        if (!_cache.TryGetValue(id, out GameState? state))
             return NotFound(new { error = "Game not found" });
 
-        game!.MakeTurn(request.Cell);
-        if (game.ReadCurrentPlayerLevel() != Game.HUMAN) game.MakeTurn(); // Сходить за бота
+        if (state!.Game.ReadWhoseTurn() == Game.X) state.ConnectedPlayers |= Game.X;
+        else state.ConnectedPlayers |= Game.O;
 
-        _cache.Set(id, game, TimeSpan.FromMinutes(30));
+        state.Game.MakeTurn(request.Cell);
+        if (state.Game.ReadCurrentPlayerLevel() != Game.HUMAN) state.Game.MakeTurn(); // Сходить за бота
+
+        _cache.Set(id, state, TimeSpan.FromMinutes(30));
 
         return GetGame(id);
     }
 
-    private static string FormatBoard(Game game)
+    private static string FormatBoard(GameState game)
     {
         string board = "";
         for (int i = 0; i < 9; i++)
         {
-            uint cell = game.ReadCellType(i);
+            uint cell = game.Game.ReadCellType(i);
             board += cell == Game.EMPTY ? '-' : cell == Game.X ? 'x' : cell == Game.O ? 'o' : '=';
         }
         return board;
@@ -80,10 +83,15 @@ public class GameController(IMemoryCache cache) : ControllerBase
 }
 
 
+public record GameState(Game Game, int ConnectedPlayers)
+{
+    public int ConnectedPlayers = ConnectedPlayers;
+}
+
 public record NewGameRequest(uint XLevel, uint OLevel);
 public record NewGameResponse(Guid Id);
 
-public record GameResponse(string Board, int Turn, uint Winner);
+public record GameResponse(string Board, int Turn, uint Winner, int ConnectedPlayers);
 public record MakeTurnRequest(int Cell);
 
 public record ListGamesResponse(Guid[] Ids);
