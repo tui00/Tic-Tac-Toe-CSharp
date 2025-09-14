0c1022e8172179e66b834128d290ca3d8dad66dc
Егор <egor5f421@mail.ru>
2025-09-14 06:54:36 +0300

    refactor(api): make GameState record internal for better encapsulation

--- TicTacToe.Api/DTO/GameState.cs
+++ TicTacToe.Api/DTO/GameState.cs
@@ -2,7 +2,7 @@ using TicTacToe.Core;
 
 namespace TicTacToe.Api.Dto;
 
-public record GameState(Game Game, int ConnectedPlayers)
+internal record GameState(Game Game, int ConnectedPlayers)
 {
     public int ConnectedPlayers = ConnectedPlayers;
 }
